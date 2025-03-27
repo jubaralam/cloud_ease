39de47b99fd3ca8b34b445bd443fd02b5297230e
Zubair Alam <zubairalam@Zubairs-MacBook-Air.local>
2025-03-27 17:56:14 +0530

    ..'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-// import PublicRoutes from './routes/PublicRoutes'
-import Login from "./pages/Login";
+
 import AddAccount from "./pages/AddAccount";
 const App = () => {
   return (
